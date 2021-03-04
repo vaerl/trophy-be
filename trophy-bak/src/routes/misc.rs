567e94506ecff6173c1474120bedf9b6a857e1a5
vaerl <lukas@mlel.dev>
2021-03-04 21:43:31 +0100

    Removed index, added /ping.

--- trophy-bak/src/routes/misc.rs
+++ trophy-bak/src/routes/misc.rs
@@ -2,61 +2,16 @@ use actix_web::{get, post, web, HttpResponse, Responder};
 use anyhow::Result;
 use sqlx::{Error, PgPool};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    // This function provides an overview of all the different routes.
-    // TODO think about removing this -> documented through ./http-files
-    HttpResponse::Ok().body(
-        r#"
-        KL-Bak
-
-        This backend is currently under development
-
-        Available routes:
-
-        Team
-        GET /teams -> list of all teams
-        GET /teams/amount -> amount of all teams
-        POST /teams -> create new team, body: { "id": 1, "name": "test", "gender": "male", "points": 0 }
-        GET /teams/id -> find team
-        PUT /teams/id -> update team, body: { "id": 1, "name": "test2", "gender": "mixed", "points": 0 }
-        DELETE /teams/id -> delete team
-        GET /teams/id/pending -> pending games
-        GET /teams/id/pending/amount -> amount of pending games
-        GET /teams/id/finished -> finished games
-
-        Game
-        GET /games -> list of all games
-        GET /games/amount -> amount of all games
-        POST /games -> create new game, body: { "id": 1, "name": "name", "kind": "time" }
-        GET /games(id) -> find game
-        PUT /games/id -> update game, body: { "id": 1, "name": "name2", "kind": "time" }
-        DELETE /games -> delete game, body: { "id": 1, "name": "name2", "kind": "time" }
-        GET /games/id/pending -> pending teams
-        GET /games/id/pending/amount -> amount of pending teams
-        GET /games/id/finished -> finished teams
-
-        Result
-        GET /outcomes -> get all outcomes
-        PUT /outcomes -> update outcome, body: { "game_id": 1, "team_id": 1, "data": "text"}
-        GET /outcomes/games/{id} -> get outcomes for game
-        GET /outcomes/teams/{id} -> get outcomes for team
-
-        Evaluate
-        POST /eval/game_id -> checks if game is done and then evaluates said game
-        GET /eval -> builds an .xlsx-file and serves it
-
-        Miscellaneous
-        GET / -> this index
-        POST /reset -> reset database
-    "#,
-    )
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    debug!("Received new request: ping.");
+    HttpResponse::Ok().body("pong")
 }
 
 #[post("/reset/database")]
 async fn reset_database(db_pool: web::Data<PgPool>) -> impl Responder {
     // This resets the database COMPLETELY - use with care!
-    // TODO remove this (or at least comment it) before moving to production
+    // TODO remove this (at least comment it) before moving to production
     warn!("Received new request: reset database.");
     match reset_database_wrapper(db_pool.get_ref()).await {
         Ok(()) => HttpResponse::Ok().body("Successfully reset database."),
@@ -78,13 +33,7 @@ async fn reset_database_wrapper(pool: &PgPool) -> Result<(), Error> {
     tx.commit().await
 }
 
-#[get("/ping")]
-async fn ping() -> impl Responder {
-    debug!("Received new request: ping.");
-    HttpResponse::Ok().body("Pong.")
-}
-
 pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(index);
+    cfg.service(ping);
     cfg.service(reset_database);
 }
