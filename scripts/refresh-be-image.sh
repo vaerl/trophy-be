f2042c12e2e214e8fbdd86b5f6bef90ea4d888cc
vaerl <lukas@mlel.dev>
2021-06-06 11:09:40 +0000

    Update script.

--- scripts/refresh-be-image.sh
+++ scripts/refresh-be-image.sh
@@ -1,6 +1,6 @@
-docker stop eval-be
-docker rm eval-be
-docker rmi eval-be
-docker build -t eval-be:latest ./be
-docker run -p 5432:5432 --name eval-be -t eval-be
-docker start eval-be
+docker stop trophy-be
+docker rm trophy-be
+docker rmi trophy-be
+docker build -t trophy-be:latest ./be
+docker run -p 8080:8080 --name trophy-be -t trophy-be
+docker start trophy-be
