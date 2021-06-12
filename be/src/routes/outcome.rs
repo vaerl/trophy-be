52ab225ddf25e3e23eb90d4917ad531831f836d8
vaerl <lukas@mlel.dev>
2021-06-12 12:05:23 +0000

    Restrict outcome-access to referees.

--- be/src/routes/outcome.rs
+++ be/src/routes/outcome.rs
@@ -2,7 +2,7 @@ use actix_web::{get, put, web};
 use sqlx::PgPool;
 
 use crate::{
-    model::{History, Outcome, OutcomeVec, UserRole, UserToken},
+    model::{CustomError, History, Outcome, OutcomeVec, User, UserRole, UserToken},
     ApiResult,
 };
 
@@ -15,8 +15,9 @@ async fn find_all_outcomes(token: UserToken, db_pool: web::Data<PgPool>) -> ApiR
     Outcome::find_all(db_pool.get_ref()).await
 }
 
-#[put("/outcomes")]
-async fn create_outcome(
+/// Outcomes are automatically initialized , thus we only need an update-method().
+#[put("/outcomes/{id}")]
+async fn update_outcome(
     token: UserToken,
     outcome: web::Json<Outcome>,
     db_pool: web::Data<PgPool>,
@@ -24,14 +25,31 @@ async fn create_outcome(
     let user = token
         .try_into_authorized_user(vec![UserRole::Admin, UserRole::Referee], db_pool.get_ref())
         .await?;
+
     History::info(
         user.id,
-        format!("create outcome"),
+        format!("update outcome"),
         outcome.to_string(),
         db_pool.get_ref(),
     )
     .await?;
-    Outcome::update(outcome.into_inner(), db_pool.get_ref()).await
+
+    match user.role {
+        UserRole::Admin => Outcome::update(outcome.into_inner(), db_pool.get_ref()).await,
+        UserRole::Referee => {
+            // if the user is a referee, check if he is accessing the correct game
+            if outcome.game_id
+                == User::find_game_for_ref(user.id, db_pool.get_ref())
+                    .await?
+                    .id
+            {
+                Outcome::update(outcome.into_inner(), db_pool.get_ref()).await
+            } else {
+                Err(CustomError::AccessDeniedError)
+            }
+        }
+        UserRole::Visualizer => Err(CustomError::AccessDeniedError),
+    }
 }
 
 #[get("/outcomes/teams/{id}")]
@@ -40,6 +58,7 @@ async fn find_all_outcomes_for_team(
     team_id: web::Path<i32>,
     db_pool: web::Data<PgPool>,
 ) -> ApiResult<OutcomeVec> {
+    // only admins should be able to access this information
     let user = token
         .try_into_authorized_user(vec![UserRole::Admin], db_pool.get_ref())
         .await?;
@@ -59,7 +78,7 @@ async fn find_all_outcomes_for_game(
     db_pool: web::Data<PgPool>,
 ) -> ApiResult<OutcomeVec> {
     let user = token
-        .try_into_authorized_user(vec![UserRole::Admin], db_pool.get_ref())
+        .try_into_authorized_user(vec![UserRole::Admin, UserRole::Referee], db_pool.get_ref())
         .await?;
     let game_id = game_id.into_inner();
     History::debug(
@@ -68,12 +87,28 @@ async fn find_all_outcomes_for_game(
         db_pool.get_ref(),
     )
     .await?;
-    Outcome::find_all_for_game(game_id, db_pool.get_ref()).await
+
+    match user.role {
+        UserRole::Admin => Outcome::find_all_for_game(game_id, db_pool.get_ref()).await,
+        UserRole::Referee => {
+            // if the user is a referee, check if he is accessing the correct game
+            if game_id
+                == User::find_game_for_ref(user.id, db_pool.get_ref())
+                    .await?
+                    .id
+            {
+                Outcome::find_all_for_game(game_id, db_pool.get_ref()).await
+            } else {
+                Err(CustomError::AccessDeniedError)
+            }
+        }
+        UserRole::Visualizer => Err(CustomError::AccessDeniedError),
+    }
 }
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(find_all_outcomes);
-    cfg.service(create_outcome);
+    cfg.service(update_outcome);
     cfg.service(find_all_outcomes_for_game);
     cfg.service(find_all_outcomes_for_team);
 }
