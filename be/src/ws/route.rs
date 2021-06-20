e632b2f4828827bdccf6c6330448d0de217e8f3b
vaerl <lukas@mlel.dev>
2021-06-20 10:57:47 +0000

    Secure connections to /socket.

--- be/src/ws/route.rs
+++ be/src/ws/route.rs
@@ -5,32 +5,35 @@ use actix_web::{
     HttpRequest, HttpResponse,
 };
 use actix_web_actors::ws;
+use sqlx::PgPool;
 
-use crate::ApiResult;
+use crate::{
+    model::{LogUserAction, UserRole, UserToken},
+    ApiResult,
+};
 
 use super::{lobby::Lobby, ws::WsConn};
 
-// NOTE to use this socket, forward the app's port!
+// NOTE to use this socket, forward the app's port in VSCode!
 #[get("/socket")]
 pub async fn start_connection(
     req: HttpRequest,
     stream: Payload,
+    token: UserToken,
+    db_pool: web::Data<PgPool>,
     srv: Data<Addr<Lobby>>,
 ) -> ApiResult<HttpResponse> {
-    // TODO check for role
-    // TODO log connections with user_id -> probably don't use history!
-    debug!("User connected to socket.");
+    let _user = token
+        .try_into_authorized_user(
+            vec![UserRole::Admin, UserRole::Referee, UserRole::Visualizer],
+            db_pool.get_ref(),
+        )
+        .await?
+        .log_action(format!("connected to socket."), db_pool.get_ref());
     let ws = WsConn::new(srv.get_ref().clone());
 
     let resp = ws::start(ws, &req, stream)?;
     Ok(resp)
-
-    // TODO use only one room that everybody can listen to
-    // -> send messages when I receive CREATE, DELETE or UPDATE at the REST-endpoints
-    // -> send only refresh-messages: { subject: "Team" } -> only refresh teams here
-
-    // TODO maybe keep room-architecture for future uses???
-    // -> I could generate a Uuid and pass everybody to that room!
 }
 
 pub fn init(cfg: &mut web::ServiceConfig) {
