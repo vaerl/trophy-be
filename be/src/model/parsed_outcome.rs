b15bb34c21c7509037871b3415411631568f84da
vaerl <lukas@mlel.dev>
2022-05-28 22:40:48 +0200

    Correctly parse times.

--- be/src/model/parsed_outcome.rs
+++ be/src/model/parsed_outcome.rs
@@ -22,7 +22,17 @@ impl ParsedOutcome {
     pub async fn from(game_kind: &GameKind, outcome: Outcome, pool: &PgPool) -> ApiResult<Self> {
         let value: Value = match game_kind {
             super::GameKind::Points => Value::Points(outcome.data.unwrap().parse::<i32>()?),
-            super::GameKind::Time => Value::Time(parse_duration(&outcome.data.unwrap())?),
+            super::GameKind::Time => {
+                let data = outcome.data.unwrap();
+                let items: Vec<&str> = data.split(":").collect();
+                assert!(
+                    items.len() == 2,
+                    "Found more than two parts when splitting a time!"
+                );
+                Value::Time(parse_duration(
+                    format!("{}min {}s", items[0], items[1]).as_str(),
+                )?)
+            }
         };
         Ok(ParsedOutcome {
             game_id: outcome.game_id,
