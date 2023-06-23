2bdc5278e84bdb4b1f0e69a934bf301e3f782b2a
vaerl <lukas@mlel.dev>
2023-06-23 22:25:36 +0200

    Allow any origin (for now).

--- be/src/main.rs
+++ be/src/main.rs
@@ -45,8 +45,10 @@ async fn main() -> Result<(), CustomError> {
         let cors = Cors::default()
             .supports_credentials()
             // explicitly allow localhost for development
-            .allowed_origin("http://localhost:3000")
-            .allowed_origin(origin.as_str())
+            // TODO roll this back once I've found the issue
+            //.allowed_origin("http://localhost:3000")
+            //.allowed_origin(origin.as_str())
+            .allow_any_origin()
             .allow_any_method()
             .allow_any_header()
             .max_age(3600);
