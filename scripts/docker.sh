62836ae18c9bb6feca1f2aa4fde0cf6ad4ed5e81
vaerl <lukas@mlel.dev>
2024-02-25 21:40:55 +0100

    Add docker-scripts.

---
+++ scripts/docker.sh
@@ -0,0 +1,14 @@
+# build the image
+docker build -t trophy-be .
+
+# build the image without any caching - caching caused some confusion early on
+docker build -t trophy-be . --no-cache
+
+# run the image with the applicable environment
+docker run --name trophy-be -p 4998:4998 -d --env-file ./.env trophy-be
+
+# run bash inside the container
+docker exec -it trophy-be bash
+
+# connect the container to the network, typically named base
+docker network connect base trophy-be
