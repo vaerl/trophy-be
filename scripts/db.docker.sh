ca2087ad1ecfed78252e0c2aa0dabcd3126ec6e3
Lukas Müller <me@lukas.run>
2024-05-09 16:00:40 +0200

    Update docker-scripts.

---
+++ scripts/db.docker.sh
@@ -0,0 +1,2 @@
+# create a new database
+docker run --name postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
--- scripts/refresh-be-image.sh
+++
@@ -1,6 +0,0 @@
-docker stop trophy-be
-docker rm trophy-be
-docker rmi trophy-be
-docker build -t trophy-be:latest ./be
-docker run -p 8080:8080 --name trophy-be -t trophy-be
-docker start trophy-be
